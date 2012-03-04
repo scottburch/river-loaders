f993df38f866d08ba4fa73383a8e1bc30ca92b11
Scott Burch <scott@toto.bulldoginfo.com>
2012-03-04 13:51:02 -0800

    updated to add the base path of require

--- loaders.js
+++ loaders.js
@@ -3,7 +3,7 @@ defineModule({name:'loaders', category:'River', description:'Loaders for css...'
     that.do_loadCss = function(data, mod) {
         var el = document.createElement('link');
         el.setAttribute('rel', 'stylesheet');
-        el.setAttribute('href', mod.modulePath + '/' + data.href);
+        el.setAttribute('href', (window.requireConfig.baseUrl || '') + mod.modulePath + '/' + data.href);
         document.getElementsByTagName("head")[0].appendChild(el);
     };
 });
